8cd4e7d4fc96170ffdcf93732590214662f3e2d5
liuxing2 <liuxing2@shoplineapp.com>
2022-10-02 14:31:07 +0800

    feat: add css plugin

--- mini-vite/src/node/plugins/index.ts
+++ mini-vite/src/node/plugins/index.ts
@@ -2,7 +2,13 @@ import { Plugin } from '../plugin'
 import { esbuildTransformPlugin } from "./esbuild";
 import { importAnalysisPlugin } from "./importAnalysis";
 import { resolvePlugin } from "./resolve";
+import { cssPliugin } from './css';
 
 export function resolvePlugins(): Plugin[] {
-  return [resolvePlugin(), esbuildTransformPlugin(), importAnalysisPlugin()];
+  return [
+    resolvePlugin(),
+    esbuildTransformPlugin(),
+    importAnalysisPlugin(),
+    cssPliugin()
+];
 }
